e136b9a3a12fe76ba72625de72b34309dbe622da
pedrogillet1 <123hackerabc@gmail.com>
2025-12-08 10:54:43 -0300

    Replace folder dropdown inline SVGs with icon components
    Updated folder dropdown to use the same icon components as file dropdown:
    - Edit button: Now uses EditIcon (was inline SVG)
    - Move button: Now uses AddIcon (was inline SVG)
    - Delete button: Now uses TrashCanIcon (was inline SVG)
    
    All icons standardized to 20x20px with consistent styling.

--- frontend/src/components/CategoryDetail.jsx
+++ frontend/src/components/CategoryDetail.jsx
@@ -2162,10 +2162,7 @@ const CategoryDetail = () => {
                                 onMouseEnter={(e) => e.currentTarget.style.background = '#F5F5F5'}
                                 onMouseLeave={(e) => e.currentTarget.style.background = 'transparent'}
                               >
-                                <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" style={{ marginRight: 10, flexShrink: 0 }}>
-                                  <path d="M11 4H4C3.46957 4 2.96086 4.21071 2.58579 4.58579C2.21071 4.96086 2 5.46957 2 6V20C2 20.5304 2.21071 21.0391 2.58579 21.4142C2.96086 21.7893 3.46957 22 4 22H18C18.5304 22 19.0391 21.7893 19.4142 21.4142C19.7893 21.0391 20 20.5304 20 20V13" stroke="#6B7280" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                                  <path d="M18.5 2.50001C18.8978 2.10219 19.4374 1.87869 20 1.87869C20.5626 1.87869 21.1022 2.10219 21.5 2.50001C21.8978 2.89784 22.1213 3.4374 22.1213 4.00001C22.1213 4.56262 21.8978 5.10219 21.5 5.50001L12 15L8 16L9 12L18.5 2.50001Z" stroke="#6B7280" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                                </svg>
+                                <EditIcon style={{ width: 20, height: 20, marginRight: 10, flexShrink: 0 }} />
                                 {t('common.edit')}
                               </button>
                               <button
@@ -2215,10 +2212,7 @@ const CategoryDetail = () => {
                                 onMouseEnter={(e) => e.currentTarget.style.background = '#F5F5F5'}
                                 onMouseLeave={(e) => e.currentTarget.style.background = 'transparent'}
                               >
-                                <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" style={{ marginRight: 10, flexShrink: 0 }}>
-                                  <path d="M12 5V19" stroke="#6B7280" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                                  <path d="M5 12H19" stroke="#6B7280" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                                </svg>
+                                <AddIcon style={{ width: 20, height: 20, marginRight: 10, flexShrink: 0 }} />
                                 {t('common.move')}
                               </button>
                               <button
@@ -2247,10 +2241,7 @@ const CategoryDetail = () => {
                                 onMouseEnter={(e) => e.currentTarget.style.background = '#FEE2E2'}
                                 onMouseLeave={(e) => e.currentTarget.style.background = 'transparent'}
                               >
-                                <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" style={{ marginRight: 10, flexShrink: 0 }}>
-                                  <path d="M3 6H5H21" stroke="#D92D20" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                                  <path d="M8 6V4C8 3.46957 8.21071 2.96086 8.58579 2.58579C8.96086 2.21071 9.46957 2 10 2H14C14.5304 2 15.0391 2.21071 15.4142 2.58579C15.7893 2.96086 16 3.46957 16 4V6M19 6V20C19 20.5304 18.7893 21.0391 18.4142 21.4142C18.0391 21.7893 17.5304 22 17 22H7C6.46957 22 5.96086 21.7893 5.58579 21.4142C5.21071 21.0391 5 20.5304 5 20V6H19Z" stroke="#D92D20" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                                </svg>
+                                <TrashCanIcon style={{ width: 20, height: 20, marginRight: 10, flexShrink: 0 }} />
                                 {t('common.delete')}
                               </button>
                             </div>,
